cc0c9022ff5b37ca1ae92969eb1c98b2bf5da49d
Mike Nitchie <nitchie@careevolution.com>
2015-11-07 17:08:52 -0500

    Tweak some config settings.

--- app/app.js
+++ app/app.js
@@ -10,7 +10,23 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+
+  // Basic logging, e.g. "Transitioned into 'post'"
+  LOG_TRANSITIONS: true,
+
+  // Extremely detailed logging, highlighting every internal
+  // step made while transitioning into a route, including
+  // `beforeModel`, `model`, and `afterModel` hooks, and
+  // information about redirects and aborted transitions
+  LOG_TRANSITIONS_INTERNAL: false,
+
+  Ember.ENV.RAISE_ON_DEPRECATION = true,
+  Ember.ENV.LOG_STACKTRACE_ON_DEPRECATION = true,
+
+  // Clearer stack traces on errors.
+  // Remove for production
+  Ember.run.backburner.DEBUG = true
 });
 
 loadInitializers(App, config.modulePrefix);
