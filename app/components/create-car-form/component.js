df5052843275ac3750022673084a980e494b4cab
Mike Nitchie <nitchie@careevolution.com>
2015-12-20 12:01:58 -0500

    Tweak css

--- app/components/create-car-form/component.js
+++ app/components/create-car-form/component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['create-car-form, form-inline'],
+	classNames: ['create-car-form form-inline'],
 
 	makes: null,
 	models: null,
