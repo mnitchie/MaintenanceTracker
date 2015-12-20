4c1539f0fbfe66ce68d0c9497d42baee7705219e
Mike Nitchie <nitchie@careevolution.com>
2015-12-20 14:29:38 -0500

    Navigatin'

--- app/router.js
+++ app/router.js
@@ -2,15 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('cars', function() {
-    this.route('car', {
-      path: ':id'
+    this.route('cars', function() {
+        this.route('car', { path: ':id' }, function() {
+          this.route('maintenance');
+          this.route('edit');
+        });
     });
-  });
 });
 
 export default Router;
